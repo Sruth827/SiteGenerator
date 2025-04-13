8d40b755c74043766f9f4e3262e494f75ee4c405
Sean Ruth <Seanruth27@gmail.com>
2025-04-12 22:21:27 -0700

    markdown delimiter and unit test

---
+++ src/splitnode.py
@@ -0,0 +1,28 @@
+from textnode import TextNode, TextType
+
+def split_node_delimiter(old_nodes, delimiter, text_type):
+    new_list = []
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT:
+            new_list.append(node)
+            continue
+        
+        lines = node.text.split(delimiter)
+
+        if len(lines) % 2 == 0:
+            raise ValueError(f"Invalid Markdown: no matching '{delimiter}'")
+
+        for i, line in enumerate(lines):
+            if i%2 == 1:
+                new_list.append(TextNode(line, text_type))
+            else:
+                        
+                if line:
+                    new_list.append(TextNode(line, TextType.TEXT))
+    return new_list
+
+
+    
+
+
+
---
+++ src/test_splitnode.py
@@ -0,0 +1,71 @@
+import unittest
+from textnode import TextNode, TextType
+from splitnode import split_node_delimiter
+
+
+
+class TestSplitNodesDelimiter(unittest.TestCase):
+    def test_basic_split(self):
+        node = TextNode("This is `code` text", TextType.TEXT)
+        new_nodes = split_node_delimiter([node], "`", TextType.CODE)
+        
+        self.assertEqual(len(new_nodes), 3)
+        self.assertEqual(new_nodes[0].text, "This is ")
+        self.assertEqual(new_nodes[0].text_type, TextType.TEXT)
+        self.assertEqual(new_nodes[1].text, "code")
+        self.assertEqual(new_nodes[1].text_type, TextType.CODE)
+        self.assertEqual(new_nodes[2].text, " text")
+        self.assertEqual(new_nodes[2].text_type, TextType.TEXT)
+
+    def test_multiple_delimiters(self):
+        node = TextNode("This `has` multiple `code` blocks", TextType.TEXT)
+        new_nodes = split_node_delimiter([node], "`", TextType.CODE)
+        
+        self.assertEqual(len(new_nodes), 5)
+        self.assertEqual(new_nodes[1].text, "has")
+        self.assertEqual(new_nodes[1].text_type, TextType.CODE)
+        self.assertEqual(new_nodes[3].text, "code")
+        self.assertEqual(new_nodes[3].text_type, TextType.CODE)
+
+    def test_italic_split(self):
+        node = TextNode("This is _italic_ text", TextType.TEXT)
+        new_nodes = split_node_delimiter([node], "_", TextType.ITALIC)
+        
+        self.assertEqual(len(new_nodes), 3)
+        self.assertEqual(new_nodes[1].text, "italic")
+        self.assertEqual(new_nodes[1].text_type, TextType.ITALIC)
+
+    def test_bold_split(self):
+        node = TextNode("This is **bold** text", TextType.TEXT)
+        new_nodes = split_node_delimiter([node], "**", TextType.BOLD)
+        
+        self.assertEqual(len(new_nodes), 3)
+        self.assertEqual(new_nodes[1].text, "bold")
+        self.assertEqual(new_nodes[1].text_type, TextType.BOLD)
+
+    def test_no_split(self):
+        node = TextNode("No delimiters here", TextType.TEXT)
+        new_nodes = split_node_delimiter([node], "`", TextType.CODE)
+        
+        self.assertEqual(len(new_nodes), 1)
+        self.assertEqual(new_nodes[0].text, "No delimiters here")
+        self.assertEqual(new_nodes[0].text_type, TextType.TEXT)
+
+    def test_unmatched_delimiter(self):
+        node = TextNode("Unmatched `delimiter", TextType.TEXT)
+        with self.assertRaises(ValueError) as context:
+            split_node_delimiter([node], "`", TextType.CODE)
+        
+        self.assertEqual(str(context.exception), "Invalid Markdown: no matching '`'")
+
+    def test_mixed_node_types(self):
+        node1 = TextNode("This is `code` text", TextType.TEXT)
+        node2 = TextNode("This is bold text", TextType.BOLD)
+        new_nodes = split_node_delimiter([node1, node2], "`", TextType.CODE)
+        
+        self.assertEqual(len(new_nodes), 4)
+        self.assertEqual(new_nodes[3].text, "This is bold text")
+        self.assertEqual(new_nodes[3].text_type, TextType.BOLD)
+
+if __name__ == "__main__":
+    unittest.main()
